fc47a145f4baf6f7604f8b9dbb408d5fc50e6f36
maish79 <maishsupa@yahoo.com>
2023-06-07 08:49:19 +0000

    Working with first View, Register and login of user

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+
+# from .views import login, register, logout, ProductList, Profile, ProductCreate, ProductUpdate, ProductDelete
+from django.contrib.auth import views as auth_views
+
+app_name = 'account'
+urlpatterns = [
+
+
+    
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.contrib.auth import models
+from django.shortcuts import redirect, render
+from django.urls.base import reverse_lazy
+from .models import User
+from shop.models import *
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('account:home')
+        else:
+            messages.error(request, 'Invalid Login')
+            return redirect('account:login')
+    return render(request, 'account/login.html')
+    
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']  
+        confirm_password = request.POST['confirm_password'] 
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Someone with this username already exist!')
+                return redirect('account:register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'This Email is already exist')
+                    return redirect('account:register')
+                else:
+                    user = User.objects.create_user(first_name = first_name, last_name=last_name, username=username, email=email, password=password)
+                    user.save()
+                    messages.success(request, 'You Successfully Created account')
+                    return redirect('account:login')
+        else:
+            messages.error(request, 'Passwords are not same')
+
+    return render(request, 'account/register.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You! successfully Logged out')
+        return redirect('account:login')
+    return redirect('account:login')
