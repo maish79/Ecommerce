817ac019a210941c028d621a458e7e7df31c7f9a
maish79 <maishsupa@yahoo.com>
2023-06-07 13:35:35 +0000

    some code clean up toke place

--- shop/urls.py
+++ shop/urls.py
@@ -6,14 +6,9 @@ urlpatterns = [
     path('', Home, name = 'home'),
     path('product/<slug:slug>/', detail, name = 'product'),
     path('category/<slug:slug>/', category, name = 'category'),
-    # path('checkout/',CheckOutView.as_view(), name = 'checkout'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
-    # path('increase-quantity/<slug>/', increase_quantity, name='increase-quantity'),
-    # path('decrease-quantity/<slug>/', decrease_quantity, name='decrease-quantity'),
-    # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
     path('delete-from-cart/<int:order_item_id>/', delete_from_cart, name='delete_from_cart'),
-    # path('search/', search, name='search'),
 
 ]
