90fb933417a44f5d25480c60905599d5370a90d1
maish79 <maishsupa@yahoo.com>
2023-06-07 09:15:02 +0000

    Urls and views of shop app is updated

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from .views import  *
 app_name = 'shop'
 urlpatterns = [
     path('', Home, name = 'home'),
-    
-
+    path('product/<slug:slug>/', detail, name = 'product'),
+    path('category/<slug:slug>/', category, name = 'category'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.views import View
+from .forms import CheckOutForm, ReviewForm
+from .stripe_payment import stripe_payment
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from .models import *
@@ -22,7 +24,26 @@ def Home(request):
 
     return render(request, 'home.html', context)
 
-   
+def detail(request, slug):
+    product = get_object_or_404(Item, slug=slug)
+    all_reviews = product.reviews.filter(active=True)
+    new_review = None
+    if request.method == 'POST':
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            new_review = review_form.save(commit=False)
+            new_review.product = product
+            new_review.save()
+            return HttpResponseRedirect('')
+    else:
+        review_form = ReviewForm()
+    context = {
+        'product': product,
+        'all_reviews': all_reviews,
+        'new_review':new_review,
+        'review_form':review_form,
+    }
+    return render(request, 'detail.html', context)
 
 def category(request, slug):
     context = {
