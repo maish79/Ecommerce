984d8ebfe8e6da45881a81d210f2e2d58e86016b
maish79 <maishsupa@yahoo.com>
2023-06-07 08:59:26 +0000

    url files are created and implemented for both apps

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 
-# from .views import login, register, logout, ProductList, Profile, ProductCreate, ProductUpdate, ProductDelete
+from .views import login, register, logout
 from django.contrib.auth import views as auth_views
 
 app_name = 'account'
 urlpatterns = [
+
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 
 
     
