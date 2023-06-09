830acc6d35ea2b076465cd9c420325190c7c47c4
maish79 <maishsupa@yahoo.com>
2023-06-09 08:28:17 +0000

    path of forget password is added in urls

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
-
+from django.contrib.auth import views as auth_views
 from .views import login, register, logout, ProductList, Profile, ProductCreate, ProductUpdate, ProductDelete
-# ProductCreate, ProductUpdate, ProductDelete
 
 app_name = 'account'
 urlpatterns = [
@@ -15,5 +14,12 @@ urlpatterns = [
     path('product/update/<int:pk>', ProductUpdate.as_view(), name='product-update'),
     path('product/delete/<int:pk>', ProductDelete.as_view(), name='product-delete'),
     
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = "account/password_reset.html"), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = "account/password_reset_sent.html"), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_resent_confirm.html'), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name = 'account/password_reset_complete.html'), name='password_reset_complete'),
     
 ]
